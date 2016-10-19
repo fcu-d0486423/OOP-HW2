b4371c21416f293f7bded31e856ed49c5582d3e7
fcu-d0486423 <j.jerry240@gmail.com>
2016-10-20 00:55:26 +0800

    new

--- src/StringComparatorclass.java
+++ src/StringComparatorclass.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 public class StringComparatorclass {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboard =new Scanner(System.in);
+		do{
 		System.out.println("Enter a string1:");
 		String name=keyboard.next();
 		System.out.println("Enter a string2:");
 		String name2=keyboard.next();
 		if(name.equalsIgnoreCase(name2))
 		{
-			System.out.println("Ture");
+			System.out.println("True");
+			break;
 		}
 		else
 		{
 			System.out.println("FLAT");
 		}
+		}while(true);
 	}
-
+	
 }
